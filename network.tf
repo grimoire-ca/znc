39d358dadbcf6d2a9b6c113f2b34341b59e7801f
Owen Jacobson <owen@grimoire.ca>
2019-01-08 23:53:39 -0500

    Add port 8000 for ZNC.
    The plan is to use this port for TLS.

--- network.tf
+++ network.tf
@@ -51,6 +51,14 @@ resource "aws_security_group" "znc" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
+  ingress {
+    protocol  = "tcp"
+    from_port = 8000
+    to_port   = 8000
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
